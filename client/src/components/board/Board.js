7aa8d6cd9a3eba2fc299172d98bffc9488bcfa18
Curtis Lane <clanelmt@gmail.com>
2020-10-23 17:22:17 -0400

    refine logic for tile movement

--- client/src/components/board/Board.js
+++ client/src/components/board/Board.js
@@ -13,31 +13,44 @@ const shuffle = array => array.sort(() => Math.random() - 0.5);
 const Board = () => {
     
     const { gameState, setGameState } = useContext(GameContext)
-    let shuffledTiles = shuffle(gameState.tileNumbers)
-
+    
+    useEffect(() => {
+        let shuffledTiles = shuffle(gameState.tileNumbers)
+        setGameState({...gameState, shuffledTiles: shuffledTiles, gameOver: false})
+        console.log(gameState.shuffledTiles)
+    }, [])
+    
     const handleClick = e => {
+        const zeroIndex = gameState.shuffledTiles.indexOf(0)
         const tileVal = e.target.attributes.value["value"]
-        const tileValIndex = shuffledTiles.indexOf(parseInt(tileVal))
+        const tileValIndex = gameState.shuffledTiles.indexOf(parseInt(tileVal))
 
         // if clicked tile is next to 0
-        moveTile(tileValIndex)
-
+        if (Math.abs(zeroIndex - tileValIndex) === 3 || Math.abs(zeroIndex - tileValIndex) === 1) {
+            moveTile(tileValIndex, zeroIndex)
+        }
+        
     }
 
-    const moveTile = (tileValIndex) => {
-        const zeroIndex = shuffledTiles.indexOf(0)
-        const a = shuffledTiles[tileValIndex];
+    const moveTile = (tileValIndex, zeroIndex) => {
+        const a = gameState.shuffledTiles[tileValIndex];
         
-        shuffledTiles[tileValIndex] = shuffledTiles[zeroIndex];
-        shuffledTiles[zeroIndex] = a;
-        console.log(tileValIndex, zeroIndex)
+        let s = gameState.shuffledTiles
+        s[tileValIndex] = s[zeroIndex];
+        s[zeroIndex] = a;
+        setGameState({...gameState, shuffledTiles: s})
+    }
+
+    const handleGameOver = () => {
+        setGameState({...gameState, gameOver: true})
+        alert("Game over!")
     }
 
     return (
         <Card className="bg-dark" style={{ width: "100%", padding: "-6.25% 0 -6.25% 0", position: "relative" }}>
             <Card.Body>
                 <div style={{position: "relative", bottom: 0}} className="row no-gutters">
-                    {shuffledTiles.map(t => {
+                    {gameState.shuffledTiles.map(t => {
                         return t > 0 ?
                             (
                                 <div key={t} value={t} className="col-4">
--- client/src/utils/useGameModel.js
+++ client/src/utils/useGameModel.js
@@ -3,9 +3,10 @@ import { useState } from "react";
 const useGameModel = () => {
     const [gameState, setGameState] = useState(
         {
-            tileNumbers: [0, 1, 2, 3, 4, 5, 6, 7, 8]
-        },
-        {
+            tileNumbers: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+        
+            shuffledTiles: [],
+        
             gameOver: false
         }
     )
